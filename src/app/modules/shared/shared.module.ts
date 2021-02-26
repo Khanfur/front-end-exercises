1226727123608e4e24e265a2a487413b613dd3c0
Brandon <brandon.tromp@pxlwidgets.com>
2021-02-26 18:27:45 +0100

    added ui module

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,14 +2,18 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { UiComponentsModule } from '@app-components/ui/ui.module';
+
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
+        UiComponentsModule,
     ],
     exports: [
         CommonModule,
         FormsModule,
+        UiComponentsModule,
     ],
 })
 export class SharedModule {
