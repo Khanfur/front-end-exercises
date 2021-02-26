b3cd00ad004cd82c0d01b127848f15c2b16ee7f5
Brandon <brandon.tromp@pxlwidgets.com>
2021-02-26 18:31:37 +0100

    generated header component

--- src/app/components/ui/ui.module.ts
+++ src/app/components/ui/ui.module.ts
@@ -2,9 +2,15 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { HeaderComponent } from '@app-components/ui/header/header.component';
+
 @NgModule({
-    declarations: [],
-    exports: [],
+    declarations: [
+        HeaderComponent,
+    ],
+    exports: [
+        HeaderComponent,
+    ],
     imports: [
         CommonModule,
         RouterModule,
